d432c1ca281d6c256fba6fc289dbc1e1323c073e
Mahmud <mahmudislamoly99@gmail.com>
2020-01-14 11:00:26 +0600

    Initial commit

---
+++ app/src/main/java/com/example/testspinnerdemo/MySingletone.java
@@ -0,0 +1,20 @@
+package com.example.testspinnerdemo;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MySingletone {
+    private static MySingleton mInstance;
+    private RequestQueue mRequestQueue;
+    private static Context mCtx;
+
+    private MySingleton(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+}
+
+    private RequestQueue getRequestQueue() {
+    }
---
+++ app/src/main/java/com/example/testspinnerdemo/State.java
@@ -0,0 +1,22 @@
+package com.example.testspinnerdemo;
+
+import java.util.List;
+
+public class State {
+
+    private String stateName;
+    private List<String> cities;
+
+    public State(String stateName, List<String> cities) {
+        this.stateName = stateName;
+        this.cities = cities;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public List<String> getCities() {
+        return cities;
+    }
+}
